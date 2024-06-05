0c47abe081d53d54e4bca8216dab24e1ae0c2014
justeozan <justeoz.an+projects@gmail.com>
2024-06-05 11:19:13 +0200

    add library's

--- include/cub3D.h
+++ include/cub3D.h
@@ -6,14 +6,15 @@
 /*   By: ozasahin <ozasahin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 11:10:14 by ozasahin          #+#    #+#             */
-/*   Updated: 2024/06/05 11:10:47 by ozasahin         ###   ########.fr       */
+/*   Updated: 2024/06/05 11:18:46 by ozasahin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
 
-
-
+# include <stdlib.h>
+# include <unistd.h>
+# include <math.h>
 
 #endif
